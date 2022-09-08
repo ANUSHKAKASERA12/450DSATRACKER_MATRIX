455c6fd3bac06ba98ed08d37b63224e2bad36d59
ANUSHKA <111751174+ANUSHKAKASERA12@users.noreply.github.com>
2022-09-08 14:01:58 +0530

    Create 4.PrintAllEmenetsUsingRowwiseSortedMatrix.java

---
+++ 4.PrintAllEmenetsUsingRowwiseSortedMatrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    int[][] sortedMatrix(int N, int Mat[][]) {
+        // code here
+        int min=Mat[0][0];
+        int max=Mat[N-1][N-1];
+        int arr[]= new int[N*N];
+        int k=0;
+        int Result[][]= new int [N][N];
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                arr[k]=Mat[i][j];
+                k++;
+            }
+        }
+        int k1=0;
+        Arrays.sort(arr);
+         for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                Result[i][j]=arr[k1];
+                k1++;
+    }}
+    return Result;
+}}
